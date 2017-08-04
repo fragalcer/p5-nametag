84af4952dd29ed1366f659bddf4ae563c3e7bd65
franciscogallardo <fragalcer@gmail.com>
2017-08-04 13:29:33 +1000

    Implemented Custom Font. Created a cool gradient background for my name.

--- sketch.js
+++ sketch.js
@@ -1,28 +1,61 @@
+
+
+var customFont;
+
+// Constants
+var Y_AXIS = 1;
+var X_AXIS = 2;
+var b1, b2, c1, c2;
+
+function preload() {
+    customFont = loadFont("century_gothic.ttf");
+}
+
 function setup() {
     // create the canvas
     createCanvas(800, 600);
 
-    // make the text nice and big - adjust theSize parameter
-    // to make *your* name fit nicely on the nametag
+    // Define colors
+    c1 = color(20,232,100);
+    c2 = color(1,126,213);
+
+    noLoop();
+
     textSize(150);
 
-    // draw a border to help you see the size
-    // this isn't compulsory (remove this code if you like)
+    // Image Border
     strokeWeight(5);
     rect(0, 0, width, height);
 }
 
 function draw() {
-    // your cool nametag code goes here
 
-    // replace my name with your name!
-    text("Ben", 100, height-100);
+    setGradient(50, 373, 700, 140, c2, c1, X_AXIS);
+
+    // My name
+    textFont(customFont);
+    // text.noStroke();
+    // text.fill(100);
+    noStroke();
+    text("Francisco", 55, height-50);
+
 }
 
-// this function is here so that when you hit the spacebar, the current version
-// of the nametag sketch will be saved to your downloads folder
+// save to downloads folder
 function keyTyped() {
     if (key === " ") {
         saveCanvas("nametag.png");
+    }
+}
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+
+    if (axis === X_AXIS) {  // Left to right gradient
+        for (var i = x; i <= x+w; i++) {
+            var inter = map(i, x, x+w, 0, 1);
+            var c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(i, y, i, y+h);
+        }
     }
 }
